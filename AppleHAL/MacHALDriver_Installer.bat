359ae47f3048b8c709a48a03ca49d9bab5949fd8
vinaypundith <77790778+vinaypundith@users.noreply.github.com>
2023-06-15 00:04:41 -0400

    Delete old MacHALDriver installer

--- AppleHAL/MacHALDriver_Installer.bat
+++
@@ -1,7 +0,0 @@
-@echo MacHALDriver.inf Installer
-
-start "" "%SYSTEMDRIVE%\Users\%USERNAME%\Downloads\Valor_Lite-(Dont-Delete-Backup)\Resources\Drivers\Pre_Downloaded\BootCamp\Drivers\Apple\AppleHAL\dpinst.exe" /a /path %SYSTEMDRIVE%\Users\%USERNAME%\Downloads\Valor_Lite-(Dont-Delete-Backup)\Resources\Drivers\Pre_Downloaded\BootCamp\Drivers\Apple\AppleHAL\
-
-Pause:
-
-@echo off
